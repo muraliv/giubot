15ae22e2ae150fb11f5ee7eb083feeff730f69c9
Murali Veeraiyan <murali.veeraiyan@gmail.com>
2015-12-03 17:39:19 +0530

    Updated app.rb

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
-require "sinatra"
-require "httparty"
-require "json"
+require 'sinatra'
+require 'httparty'
+require 'json'
 
-post "/gateway" do
-	message = params[:text].gsub(params[:trigger_word],"").strip
-	action, repo = message.split("_").map {|c| c.strip.downcase }
+post '/gateway' do
+	message = params[:text].gsub(params[:trigger_word], '').strip
+	action, repo = message.split('_').map {|c| c.strip.downcase }
 	repo_url = "https://api.github.com/repos/#{repo}"
-
 	case action
-		when "issues"
-			resp = HTTParty.get(repo_url)
-			resp = JSON.parse resp.body
-			respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+		when 'issues'
+		resp = HTTParty.get(repo_url)
+		resp = JSON.parse resp.body
+		respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
 		end
-	end	
+	end
 end
 
 def respond_message message
